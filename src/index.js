71b68715464fd36b35110221a5628bb0f5f12888
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2022-08-04 16:19:36 +0530

    feat: added inline asset.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from "./App";
+import Logo from './logo.svg'
 
 const root = document.getElementById("root");
 
 root.innerHTML = `
+<img src="${Logo}" alt="builder icon" />
 <h1> learning web tooling today </h1>
 ${App({ name: "Tanay" })}
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,13 @@ const webpackConfig = {
 			{
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"]
+			},
+
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset"
 			}
+
 		]
 	},
 
